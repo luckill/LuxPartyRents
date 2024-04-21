50b8921568119586e69abd478077a19f13961c82
Wojtek <andreaszignago@gmail.com>
2024-04-21 16:43:57 -0700

    Jira #74 & #65
    adding pages for the user cart, and user account details page, as well as updating my  style sheet.

--- SeniorProject/src/main/java/com/example/SeniorProject/MvcConfiguration.java
+++ SeniorProject/src/main/java/com/example/SeniorProject/MvcConfiguration.java
@@ -17,5 +17,7 @@ public class MvcConfiguration implements WebMvcConfigurer
         registry.addViewController("/rental").setViewName("rentals");
         registry.addViewController("/order").setViewName("UserOrder_page");
         registry.addViewController("/products").setViewName("products");
+        registry.addViewController("/UserDetails_page").setViewName("UserDetails_page");
+        registry.addViewController("/UserCart_page").setViewName("UserCart_page");
     }
 }
